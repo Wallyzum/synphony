f16c0443fece9fcded0643e7e3dcfec185cacfb3
Pablo Montenegro <wallyzum@gmail.com>
2023-05-25 20:44:50 -0300

    first Reame.MD

---
+++ README.md
@@ -0,0 +1,58 @@
+Automation Test challenge
+===
+
+## Papar Information
+
+- Authors:  Montenegro Pablo
+
+## Install & Dependence
+- node
+- npm
+
+- install dependencies
+  ```
+  npm install 
+  ```
+
+
+## Use
+- run all
+  ```
+  npm test
+  ```
+- run by file
+  ```
+  npx wdio wdio.conf.ts --spec ./test/specs/<fileName>.ts
+  ```
+
+
+## Directory Hierarchy
+```
+|—— .gitignore
+|—— .gitlab-ci.yml
+|—— allure-report
+|—— allure-result
+|—— node_modules
+|—— package-lock.json
+|—— package.json
+|—— test
+|    |—— pageobjects
+|        |—— login.page.ts
+|        |—— page.ts
+|        |—— product.page.ts
+|    |—— specs
+|        |—— backEnd.ts
+|        |—— frontEnd.e2e.ts
+|—— tsconfig.json
+|—— wdio.conf.ts
+```
+
+## Reports
+- generate reports
+  ```
+  allure generate
+  ```
+- open reports
+  ```
+  allure open
+  ```
--- test/specs/backEnd.ts
+++ test/specs/backEnd.ts
@@ -1,19 +1,28 @@
-import axios from 'axios'
+import axios from 'axios';
+
+interface Entry {
+  API: string;
+  Description: string;
+  Auth?: string;
+  HTTPS: boolean;
+  Cors: string;
+  Link: string;
+  Category: string;
+}
 
 describe('API Test', () => {
   it('should retrieve objects with Category "Authentication & Authorization"', async () => {
-    try {
-      const response = await axios.get('https://api.publicapis.org/entries');
-      const data = response.data;
-      const filteredObjects = data.entries.filter(entry =>
-        entry.Category === 'Authentication & Authorization'
-      );
-      const count = filteredObjects.length;
-      console.log(`Number of objects with Category "Authentication & Authorization": ${count}`);
-      console.log('Found objects:');
-      filteredObjects.forEach(obj => console.log(obj));
-    } catch (error) {
-      console.error('An error occurred during the API test:', error);
-    }
+    
+    (async () => {
+      try {
+        const { data } = await axios.get('https://api.publicapis.org/entries');
+        const entries: Entry[] = data.entries;
+        const foundEntries: Entry[] = entries.filter(e => e.Category === 'Authentication & Authorization');
+        const count = foundEntries.length;
+        console.log(`${count} entries found:`, foundEntries);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
   });
 });
